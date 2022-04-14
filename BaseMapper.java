51433b42a05defca48c678f234eed370f83f4c6e
zhangchuangiie <40593174+zhangchuangiie@users.noreply.github.com>
2022-04-14 10:25:22 +0800

    Add files via upload

--- BaseMapper.java
+++ BaseMapper.java
@@ -6,16 +6,17 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @Mapper
 public interface BaseMapper {
 
     List<LinkedHashMap<String, Object>> select(String sql,Object ...args);
-    //List<LinkedHashMap<String, Object>> selectPage(String sqlStr,int pageNum, int pageSize);
     long count(String sql,Object ...args);
     LinkedHashMap<String, Object> get(String sql,Object ...args);
     int insert(String sql,Object ...args);
+    int insertForID(Map map,Object ...args);
     int update(String sql,Object ...args);
     int delete(String sql,Object ...args);
     int execute(String sql,Object ...args);
