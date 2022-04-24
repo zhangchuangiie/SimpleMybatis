16fe20707dd67662d944b7280c829cb41d4bb96c
zhangchuangiie <40593174+zhangchuangiie@users.noreply.github.com>
2022-04-24 15:36:59 +0800

    Add files via upload

--- BaseMapper.java
+++ BaseMapper.java
@@ -1,6 +1,8 @@
 package com.example.demo.mapper;
 
 import com.example.demo.entity.User;
+import com.example.demo.trigger.aspect.BaseMapperDecorator;
+import com.example.demo.trigger.aspect.SignCheck;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -11,16 +13,25 @@ import java.util.Map;
 @Repository
 @Mapper
 public interface BaseMapper {
-
+    @BaseMapperDecorator("")
     List<LinkedHashMap<String, Object>> select(String sql,Object ...args);
+    @BaseMapperDecorator("")
     long count(String sql,Object ...args);
+    @BaseMapperDecorator("")
     LinkedHashMap<String, Object> get(String sql,Object ...args);
+    @BaseMapperDecorator("")
     int insert(String sql,Object ...args);
+    @BaseMapperDecorator("")
     int insertForID(String sql,Map map,Object ...args);
+    @BaseMapperDecorator("")
     int update(String sql,Object ...args);
+    @BaseMapperDecorator("")
     int delete(String sql,Object ...args);
+    @BaseMapperDecorator("")
     int execute(String sql,Object ...args);
+    @BaseMapperDecorator("")
     int executeBatch(List<String> sql,Object ...args);
+    @BaseMapperDecorator("")
     List<LinkedHashMap<String, Object>> call(String sql,Map map,Object ...args);
 }
 
