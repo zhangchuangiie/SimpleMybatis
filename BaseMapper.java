afd45e37458058ade04676c10cd2dd0db92aabba
zhangchuangiie <40593174+zhangchuangiie@users.noreply.github.com>
2022-04-26 17:25:47 +0800

    Add files via upload

--- BaseMapper.java
+++ BaseMapper.java
@@ -1,6 +1,7 @@
 package com.example.demo.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.StatementType;
 import org.springframework.stereotype.Repository;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -10,15 +11,42 @@ import java.util.Map;
 @Mapper
 public interface BaseMapper {
 
+    @Select("${sql}")
     List<LinkedHashMap<String, Object>> select(String sql,Object ...args);
+
+    @Select("${sql}")
     long count(String sql,Object ...args);
+
+    @Select("${sql}")
     LinkedHashMap<String, Object> get(String sql,Object ...args);
+
+    @Insert("${sql}")
     int insert(String sql,Object ...args);
+
+    @Options(useGeneratedKeys = true, keyProperty = "map.id")
+    @Insert("${sql}")
     int insertForID(String sql,Map map,Object ...args);
+
+    @Update("${sql}")
     int update(String sql,Object ...args);
+
+    @Delete("${sql}")
     int delete(String sql,Object ...args);
+
+    @Update("${sql}")
     int execute(String sql,Object ...args);
+
+    @Update({
+    "<script>"
+    +"<foreach item='item' index='index' collection='sql'>"
+    +"${item};"
+    +"</foreach>"
+    +"</script>"
+    })
     int executeBatch(List<String> sql,Object ...args);
+
+    @Options(statementType = StatementType.CALLABLE, useCache = false)
+    @Select("CALL ${sql}")
     List<LinkedHashMap<String, Object>> call(String sql,Map map,Object ...args);
 }
 
